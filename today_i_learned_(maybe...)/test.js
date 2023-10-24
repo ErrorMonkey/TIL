e5376e147dcd00dcdfd9a4c6d850dd314b6d71d3
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-10-24 13:11:12 +0900

    js study: prototype, object

---
+++ today_i_learned_(maybe...)/test.js
@@ -0,0 +1,56 @@
+function Person(name) {
+  let name2 = "kimkim";
+  this.name = name;
+  this.error = 0;
+  this.coding = () => {
+    this.error++;
+    console.log(`${name2}이 에러를 ${this.error}개 만들었다.`);
+  };
+}
+// 일반함수의 this는 호출된 위치에서 실행되니까,
+// this.name이 가르키는 this가 Perosn 생성자를 호출한 요소가 되는 걸까?
+// 객체 생성자 함수는 화살표 함수로 쓸 수 없는 건가?
+// 객체 내에서 선언된 변수
+// 객체를 만들 때 선언한 변수?...
+
+const kimcoder = new Person("kimcoder");
+
+kimcoder.coding();
+kimcoder.coding();
+console.log(kimcoder.name);
+console.log(kimcoder.name2);
+
+//
+
+var tester1 = {
+  "first-name": "Ung-mo",
+  "last-name": "Lee",
+  test1: "test1",
+  gender: "male",
+  1: 10,
+};
+
+console.log(tester1[1]);
+// console.log(tester1.1)
+// 얘는 왜 에러 뜸?...
+
+// 순서가 보장되지 않는다고 했는데, 같은 순서가 반복되는 건 왜일까?
+for (let prop in tester1) {
+  console.log(prop + ": " + tester1[prop]);
+}
+for (let prop in tester1) {
+  console.log(prop + ": " + tester1[prop]);
+}
+for (let prop in tester1) {
+  console.log(prop + ": " + tester1[prop]);
+}
+
+let tester1map = { ...tester1 };
+console.log("tester1map", tester1map);
+
+var array = ["one", "two"];
+array.name = "my array";
+console.log("array", array);
+array.map((el) => {
+  console.log(el);
+});
